6bfd1c5c38d578a4dec5f61b42fd763b72c169df
= <=>
2022-02-10 17:03:27 +0500

    contact form finalized

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,28 +1,43 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./contactForm.css";
 import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
   const form = useRef();
+  const [messageLength, setMessageLength] = useState(0);
+  const validMessage = messageLength < 300 ? true : false;
+  const messageLen = (e) => {
+    setMessageLength(e.target.value.length);
+  };
+  //   const [messageLength, setMessage] = useState(message.length);
 
   const sendMail = (e) => {
-    e.preventDefault();
+    if (validMessage) {
+      e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_ycetuhp",
-        "template_vvju7hr",
-        form.current,
-        "user_owYDR8Wm2vdDSHNhJn0so"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+      emailjs
+        .sendForm(
+          "service_ycetuhp",
+          "template_vvju7hr",
+          form.current,
+          "user_owYDR8Wm2vdDSHNhJn0so"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            alert("Your message has been sent! Thank You for reaching out.");
+            window.location.reload();
+          },
+          (error) => {
+            console.log(error.text);
+            alert(
+              "Something went wrong, the message was not sent. Please try again in a moment!"
+            );
+          }
+        );
+    } else {
+      alert("Message is too long! Kindly summarize it within the limit!");
+    }
   };
 
   return (
@@ -67,12 +82,17 @@ const ContactForm = () => {
             <span className="entry-container">
               <p>Message</p>
               <textarea
+                onChange={messageLen}
                 name="message"
                 required
+                id="message"
                 placeholder="Message..."
                 cols="30"
                 rows="10"
               ></textarea>
+              <h4 className={messageLength >= 300 ? "danger" : "safe"}>
+                {messageLength} / 300{" "}
+              </h4>
             </span>
             <br />
             <input id="submit" type="submit" value="SUBMIT!" />
